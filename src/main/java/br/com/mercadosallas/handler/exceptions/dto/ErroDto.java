c69c419d881d380ecbd673477d03fdfb67b36903
jvsallas <joaovictorsallas@gmail.com>
2022-09-10 18:04:05 -0300

    Add pacotes fornecedores e telefones

---
+++ src/main/java/br/com/mercadosallas/handler/exceptions/dto/ErroDto.java
@@ -0,0 +1,10 @@
+package br.com.mercadosallas.handler.exceptions.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ErroDto {
+    private String erro;
+}
---
+++ src/test/java/br/com/mercadosallas/fornecedores/FornecedoresMercadoSallasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mercadosallas.fornecedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FornecedoresMercadoSallasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
